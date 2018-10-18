659d4f36b8a7c24a8b68ad3eebc3ab1da66bb60c
tbirrell <dev@xristos.net>
2018-10-18 10:50:02 -0400

    cleaned out mindmap

--- resources/views/people/gifts/index.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="col-xs-12 section-title {{ \App\Helpers\LocaleHelper::getDirection() }}">
-  <contact-gift hash={!! $contact->hashID() !!} v-bind:gifts-active-tab="'{!! auth()->user()->gifts_active_tab !!}'"></contact-gift>
-</div>
--- resources/views/people/tasks/index.blade.php
+++
@@ -1,3 +0,0 @@
-<div class="col-xs-12 section-title {{ \App\Helpers\LocaleHelper::getDirection() }}">
-  <contact-task hash={!! $contact->hashID() !!}></contact-task>
-</div>
